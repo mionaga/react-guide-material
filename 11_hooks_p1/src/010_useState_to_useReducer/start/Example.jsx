3120780e0f24f331e3b84b0212971b8455f9dd67
ItoKona <lupinmine.1217@gmail.com>
2024-05-10 23:27:00 +0900

    about useReducer

--- 11_hooks_p1/src/010_useState_to_useReducer/start/Example.jsx
+++ 11_hooks_p1/src/010_useState_to_useReducer/start/Example.jsx
@@ -1,7 +1,34 @@
+import { useReducer } from "react";
+import { useState } from "react";
+
 const Example = () => {
-  
+  const [state, setState] = useState(0);
+  const [rstate, dispatch] = useReducer((prev, action) => {
+    if(action === '+'){
+      return ++prev;
+    }else if (action === '-'){
+      return --prev;
+    }
+  }, 0);
+
+  const countup = () => {
+    setState(prev => ++prev);
+  }; 
+  const rcountup = () => {
+    dispatch('+');
+  };
+  const rcountdown = () => {
+    dispatch('-');
+  }
+
   return (
     <>
+    <h3>{ state }</h3>
+    <button onClick={countup}>+</button>
+
+    <h3>{ rstate }</h3>
+    <button onClick={rcountup}>+</button>
+    <button onClick={rcountdown}>-</button>
     </>
   );
 };
--- 11_hooks_p1/src/025_practice_useReducer/start/Example.jsx
+++ 11_hooks_p1/src/025_practice_useReducer/start/Example.jsx
@@ -1,8 +1,99 @@
+// import { useReducer } from "react";
+
+// const CALC_OPTIONS = ["add", "minus", "divide", "multiply"];
+
+// const reducer = (state, { type, payload }) => {
+//   switch (type) {
+//     case "change":
+//       return {...state, [payload.name] : payload.value};
+//     case "add":
+//       return {...state, result: state.a + state.b};
+//     case "minus":
+//       return {...state, result: state.a - state.b};
+//     case "divide":
+//       return {...state, result: state.a * state.b};
+//     case "multiply":
+//       return {...state, result: state.a / state.b};
+//     default:
+//       throw new Error('不明な値です'); 
+//   };
+// };
+
+// const Example = () => {
+//   const initState = {
+//     a: 1,
+//     b: 2,
+//     result: 3,
+//   };
+
+//   const [state, dispatch] = useReducer(reducer, initState);
+
+//   const calculate = (e) => {
+//     dispatch({ type: e.target.value })
+//   };
+
+//   const numChangeHandler = (e) => {
+//     dispatch({ type: 'change', payload: { name: e.target.name, value: parseInt(e.target.value) } });
+//   }
+
+//   return (
+//     <>
+//     <h3>練習問題</h3>
+//     <p>useReducerを使って完成コードと同じ機能を作成してください。</p>
+//       <div>
+//         a:
+//         <input
+//           type="number"
+//           name="a"
+//           value={state.a}
+//           onChange={numChangeHandler}
+//         />
+//       </div>
+//       <div>
+//         b:
+//         <input
+//           type="number"
+//           name="b"
+//           value={state.b}
+//           onChange={numChangeHandler}
+//         />
+//       </div>
+//       <select value={state.type} onChange={calculate}>
+//         {CALC_OPTIONS.map(type => {
+//           return (
+//             <option key={type} value={type}>
+//               {type}
+//             </option>
+//           )
+//         })}
+//       </select>
+//       <h1>結果：{state.result}</h1>
+//     </>
+//   );
+// };
+
+// export default Example;
+
 import { useReducer } from "react";
 
 const CALC_OPTIONS = ["add", "minus", "divide", "multiply"];
 
-const reducer = () => {}
+const reducer = (state, action) => {
+  switch (action.pattern) {
+    case "change":
+      return { ...state, [action.payload.name]: action.payload.value };
+    case "add":
+      return { ...state, result: state.a + state.b };
+    case "minus":
+      return { ...state, result: state.a - state.b };
+    case "divide":
+      return { ...state, result: state.a * state.b };
+    case "multiply":
+      return { ...state, result: state.a / state.b };
+    default:
+      throw new Error("不明な値です");
+  }
+};
 
 const Example = () => {
   const initState = {
@@ -14,18 +105,21 @@ const Example = () => {
   const [state, dispatch] = useReducer(reducer, initState);
 
   const calculate = (e) => {
-    
+    dispatch({ pattern: e.target.value });
   };
 
   const numChangeHandler = (e) => {
-    
-  }
+    dispatch({
+      pattern: "change",
+      payload: { name: e.target.name, value: parseInt(e.target.value) },
+    });
+  };
 
   return (
     <>
-    <h3>練習問題</h3>
-    <p>useReducerを使って完成コードと同じ機能を作成してください。</p>
-      {/* <div>
+      <h3>練習問題</h3>
+      <p>useReducerを使って完成コードと同じ機能を作成してください。</p>
+      <div>
         a:
         <input
           type="number"
@@ -43,9 +137,14 @@ const Example = () => {
           onChange={numChangeHandler}
         />
       </div>
-      <select value={state.type} onChange={calculate}>
+      <select value={state.pattern} onChange={calculate}>
+        {CALC_OPTIONS.map(pattern => (
+          <option key={pattern} value={pattern}>
+            {pattern}
+          </option>
+        ))}
       </select>
-      <h1>結果：{state.result}</h1> */}
+      <h1>結果：{state.result}</h1>
     </>
   );
 };
